4039fc5c6d5082683a5ddd806a53944d43747347
Daniel-raxjal-201805639 <raxjalmy18@gmail.com>
2022-04-18 21:10:47 -0600

    Actividad 08-03-2022

---
+++ app/src/main/java/com/example/actividad08_03_2022/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.actividad08_03_2022.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
